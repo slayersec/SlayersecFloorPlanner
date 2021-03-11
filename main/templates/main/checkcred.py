1a7b806a578a1826bc6a88a170ae11906048e9d2
Daniel Saylor <dman13146@gmail.com>
2021-03-11 17:38:47 -0500

    test

--- main/templates/main/checkcred.py
+++ main/templates/main/checkcred.py
@@ -1,25 +1,18 @@
 #{% csrf_token %}
-
-
-
+import mysql.connector as mysql
 print("Hello!")
-    
-	def __init__(self):
-		import mysql.connector as mysql
-		self.db_connection = mysql.connect(host="slayersec.mysql.pythonanywhere-services.com", database="testslayersecdatabase", user="slayersec", password="13146@Data")
-
-	def login(uname,pwd):
-        sql = "SELECT * FROM login_table WHERE username = '$uname'  and password= '$psw'";
-        values = (uname,pwd)
-        user_cursor = self.db_connection.cursor()
-        try:
-            user_cursor.execute(sql, values)
-            self.db_connection.commit()
-            user_cursor.close()
-            return True
-        except:
-            user_cursor.close()
-            return False
+db_connection = mysql.connect(host="slayersec.mysql.pythonanywhere-services.com", database="testslayersecdatabase", user="slayersec", password="13146@Data")
+sql = "SELECT * FROM login_table WHERE username = '$uname'  and password= '$psw'";
+values = (uname,pwd)
+user_cursor = db_connection.cursor()
+try:
+    user_cursor.execute(sql, values)
+    self.db_connection.commit()
+    user_cursor.close()
+    return True
+except:
+    user_cursor.close()
+    return False
 
 
 
