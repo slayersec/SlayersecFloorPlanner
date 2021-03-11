94d3b8a8a409c2b1df01163895e794e02093069f
Daniel Saylor <dman13146@gmail.com>
2021-03-11 18:02:58 -0500

    scripty

--- main/templates/main/checkcred.py
+++ main/templates/main/checkcred.py
@@ -3,11 +3,12 @@ import mysql.connector as mysql
 print("Hello!")
 db_connection = mysql.connect(host="slayersec.mysql.pythonanywhere-services.com", database="testslayersecdatabase", user="slayersec", password="13146@Data")
 sql = "SELECT * FROM login_table WHERE username = '$uname'  and password= '$psw'";
+
 values = (uname,pwd)
 user_cursor = db_connection.cursor()
 try:
     user_cursor.execute(sql, values)
-    self.db_connection.commit()
+    db_connection.commit()
     user_cursor.close()
     return True
 except:
