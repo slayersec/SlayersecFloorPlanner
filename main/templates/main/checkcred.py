11c585ce684ccf009da1a7bfd422fa8b05e9cec5
Daniel Saylor <dman13146@gmail.com>
2021-03-11 17:13:09 -0500

    test

--- main/templates/main/checkcred.py
+++ main/templates/main/checkcred.py
@@ -8,9 +8,9 @@ class login():
 		import mysql.connector as mysql
 		self.db_connection = mysql.connect(host="slayersec.mysql.pythonanywhere-services.com", database="testslayersecdatabase", user="slayersec", password="13146@Data")
 
-def add_user_to_db(self,user):
+	def login(uname,pwd):
         sql = "SELECT * FROM login_table WHERE username = '$uname'  and password= '$psw'";
-        values = (user.username,user.password,user.email,user.email)
+        values = (uname,pwd)
         user_cursor = self.db_connection.cursor()
         try:
             user_cursor.execute(sql, values)
