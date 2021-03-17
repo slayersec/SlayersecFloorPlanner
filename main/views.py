fe55aac1d08db8595a4b1c4ecd83bba65edb3174
Daniel Saylor <dman13146@gmail.com>
2021-03-17 14:41:14 -0400

    Register Support

--- main/views.py
+++ main/views.py
@@ -35,8 +35,26 @@ def login_request(request):
 
 
 def register(request):
-   form = UserCreationForm
-   return render(request, "main/register.html", context={"formRegister":form})
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            login(request, user)
+            return redirect("main:homepage")
+
+        else:
+            for msg in form.error_messages:
+                print(form.error_messages[msg])
+
+            return render(request = request,
+                          template_name = "main/register.html",
+                          context={"form":form})
+
+    form = UserCreationForm
+    return render(request = request,
+                  template_name = "main/register.html",
+                  context={"form":form})
 
 def homepage(request):
    form = UserCreationForm
