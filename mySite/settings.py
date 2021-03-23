3ac2eb1f613e5d5697432560ac8a2591b546d48b
Daniel Saylor <dman13146@gmail.com>
2021-03-23 10:19:04 -0400

    yrs

--- mySite/settings.py
+++ mySite/settings.py
@@ -33,13 +33,15 @@ LOGOUT_URL = '/main/login'
 # Application definition
 
 INSTALLED_APPS = [
+    'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main.apps.MainConfig',
+    
+
 ]
 
 MIDDLEWARE = [
