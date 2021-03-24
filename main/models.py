5d6f5e0df1188907e0f57fb636d88253be85e283
Daniel Saylor <dman13146@gmail.com>
2021-03-24 15:12:08 -0400

    Models change

--- main/models.py
+++ main/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class mapdata(models.Model):
+class mapData(models.Model):
     mapName = models.CharField(max_length=200)
     data = models.CharField(max_length=20000)
     mapPublished = models.DateTimeField('date published')
+    thumbnail = models.ImageField(upload_to = 'thumbnails/')
+
 
     def __str__(self):
         return self.mapName
