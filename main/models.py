8693fb08c13f4d3083b95fbed90cc0f7e3a9e6b0
Mike Hall <mikha342@gmail.com>
2021-03-24 17:14:02 -0400

    cnage mapData to lower case mapdata

--- main/models.py
+++ main/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class mapData(models.Model):
+class mapdata(models.Model):
     mapName = models.CharField(max_length=200)
     data = models.CharField(max_length=20000)
     mapPublished = models.DateTimeField('date published')
-    thumbnail = models.ImageField(upload_to = 'thumbnails/')
+    
 
 
     def __str__(self):
