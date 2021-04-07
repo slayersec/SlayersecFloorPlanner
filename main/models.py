7542b14c849ab5ed152315e881bbeae1e5ff4b05
Daniel Saylor <dman13146@gmail.com>
2021-04-07 14:47:03 -0400

    here comes a buggy

--- main/models.py
+++ main/models.py
@@ -65,9 +65,9 @@ class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = []
 
     objects = UserManager()
-
-    def get_absolute_url(self):
-        return "/users/%i/" % (self.pk)
+    
+    def __str__(self):
+        return self.email
 
 
 
