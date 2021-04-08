57b315a07e77a28999e0a68ae7e749a58df09e4f
Daniel Saylor <dman13146@gmail.com>
2021-04-08 15:22:36 -0400

    Yes

--- main/admin.py
+++ main/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from tinymce.widgets import TinyMCE
 from django.db import models
+from users import User
 
 
 formfield_overrides = {
     models.TextField: {'widget': TinyMCE()},
     }
-#admin.site.register(mapdata)
+admin.site.register(User)
 
 # Register your models here.
