10f093a9fe48f12ba6384da17c7d6451069cc1c0
Daniel Saylor <dman13146@gmail.com>
2021-04-08 13:27:50 -0400

    Timezone fix

--- users/managers.py
+++ users/managers.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
+
+
 
 class UserManager(BaseUserManager):
 
