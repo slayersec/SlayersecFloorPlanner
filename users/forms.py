debd169106fd22cc296d606d2d3214b2c50fd1e5
Daniel Saylor <dman13146@gmail.com>
2021-04-08 15:17:59 -0400

    y

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
 from .models import User
-from django.contrib.auth import get_user_model
+
 
 
 
