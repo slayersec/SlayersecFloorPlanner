859ecb44b2eeed96a9c284ea65e6635df3b49877
Daniel Saylor <dman13146@gmail.com>
2021-04-08 11:49:52 -0400

    Yes

--- main/models.py
+++ main/models.py
@@ -5,12 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 
 
 # Create your models here.
-class mapdata(models.Model):
-    mapName = models.CharField(max_length=200)
-    data = models.CharField(max_length=20000)
-    mapPublished = models.DateTimeField('date published')
-    def __str__(self):
-        return self.mapName
+
 
 
 
