3ede1961fed7df802e9214e6e65c86a8f52a73e2
Daniel Saylor <dman13146@gmail.com>
2021-04-08 14:03:20 -0400

    RegisterFix

--- main/views.py
+++ main/views.py
@@ -44,8 +44,7 @@ def logout_request(request):
 
 def register(request):
     if request.method == "POST":
-        User = get_user_model()
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             username = form.cleaned_data.get('username')
@@ -60,7 +59,7 @@ def register(request):
                           template_name = "main/register.html",
                           context={"form":form})
 
-    form = UserCreationForm
+    form = CustomUserCreationForm
     return render(request = request,
                   template_name = "main/register.html",
                   context={"form":form})
