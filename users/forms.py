21be0b835e8c1cc59ea89a959ae62bd96c4779a8
Daniel Saylor <dman13146@gmail.com>
2021-04-08 21:02:57 -0400

    Noters

--- users/forms.py
+++ users/forms.py
@@ -17,7 +17,7 @@ class ProfileCustomizeForm(UserChangeForm):
 
     class Meta(UserChangeForm):
         model = User
-        fields = ('email', 'image', 'role', 'phone', 'age')
+        fields = ('image', 'role', 'phone', 'age', 'notes')
 
 
 class CustomUserChangeForm(UserChangeForm):
